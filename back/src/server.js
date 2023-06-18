a73b785883ec213717b35a86094f48f0b8c81e92
gnzlzp <gnzlzp86@gmail.com>
2023-06-17 19:01:19 -0500

    dotenv

--- back/src/server.js
+++ back/src/server.js
@@ -1,11 +1,12 @@
 
-const PORT = 3002;
 const express = require("express");
 const server = express();
 const cors = require('cors')
 const morgan = require("morgan");
 const router = require("./routes/index");
+require("dotenv").config();
 
+const PORT = process.env.PGPORT || 3002;
 server.use(cors())
 server.use(morgan("dev"));
 server.use(express.json());
