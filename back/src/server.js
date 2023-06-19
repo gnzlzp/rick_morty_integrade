ed4748d3cde39f24d5f41c996f3eedd0b0942e50
gnzlzp <gnzlzp86@gmail.com>
2023-06-19 11:50:02 -0500

    access by url

--- back/src/server.js
+++ back/src/server.js
@@ -10,13 +10,14 @@ const PORT = process.env.PGPORT || 3002;
 server.use(express.json());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://rick-morty-integrade.vercel.app");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 server.use("/rickandmorty", router);
+
 server.listen(PORT, () => {
   console.log("Server raised on port ", PORT);
 });
