bcc84db4ebf47fa7aa7dc1a4839197151523f688
gnzlzp <gnzlzp86@gmail.com>
2023-06-19 10:09:35 -0500

    access all

--- back/src/server.js
+++ back/src/server.js
@@ -14,7 +14,8 @@ server.use("/rickandmorty", router);
 
 // Configurar los encabezados de respuesta para CORS
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://rick-morty-integrade.vercel.app");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
